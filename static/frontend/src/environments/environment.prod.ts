23183e1fc841fb2258502d0813656013f9fa225d
Vikash Ramkumar <vikash.ramkumar@caribbean-airlines.com>
2024-08-17 23:45:11 -0400

    sighs

--- static/frontend/src/environments/environment.prod.ts
+++ static/frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://adcore-exam-hyahafdsgpg0h4c8.eastus-01.azurewebsites.net/'
+  API_URL: 'http://adcore-exam.eastus2.cloudapp.azure.com/'
 };
